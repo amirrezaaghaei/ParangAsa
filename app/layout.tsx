6baf53b4438bac33692b4cd9285a1bf1d0970dbd
AmirReza Aghaei <amirrezaaghae@gmail.com>
2024-12-08 10:51:45 +0330

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Head from "next/head";
 
 const vazirMatn = localFont({ src: "./fonts/Vazirmatn[wght].woff2" });
 
 export const metadata: Metadata = {
   title: "نیما پرنگ آسا",
-  description: "Modern & Minimal Design Portfolio",
+  description: "وبسایت شخصی نیما پرنگ‌آسا",
 };
 
 export default function RootLayout({
@@ -17,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={vazirMatn.className}>
         <ThemeProvider
           attribute="class"
